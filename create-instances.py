99a43e174e1ae0f527b8bdc018b93926d6219d24
Ranjit G <ranjitkumar.518@gmail.com>
2018-11-10 16:10:38 -0800

    Create create-instances.py

---
+++ create-instances.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import boto3
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+    ImageId='ami-1e299d7e',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro')
+print instance[0].id
