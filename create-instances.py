aaddfc03d8fdd47cad6ffb9083746984f194fe5b
Ranjit G <ranjitkumar.518@gmail.com>
2018-11-10 16:12:51 -0800

    Update create-instances.py

--- create-instances.py
+++ create-instances.py
@@ -1,4 +1,6 @@
-#aws opsworks --region us-east-1 create-instance --hostname my_instance1 --instance-type m1.large --os "Amazon Linux"
+# aws opsworks --region us-east-1 create-instance --hostname my_instance1 --instance-type m1.large --os "Amazon Linux"
+# aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
+
 #!/usr/bin/env python
 import boto3
 ec2 = boto3.resource('ec2')
