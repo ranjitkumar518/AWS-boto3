326233be79dc5fafe4e3cbafc24b3d34c1fe2a8e
Ranjit G <ranjitkumar.518@gmail.com>
2020-07-05 16:44:26 -0700

    Create get_r53_zone_id.py

---
+++ get_r53_zone_id.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python
+import boto3
+import sys
+import json
+
+## Pass zonetype public/private to get zone ID
+zone_type = sys.argv[1]
+
+    
+def get_zoneid(dns_name):
+	try :
+		client = boto3.client('route53')
+		response = client.list_hosted_zones_by_name(
+			DNSName=dns_name
+		)
+		zoneid = response['HostedZones'][0]['Id'].split('/')[2]
+		return zoneid
+	except Exception as E:
+		return None
+
+if zone_type == 'public':
+    DOMAIN_SUFFIX="abc.a.xyz.com"
+    ZONE_ID=get_zoneid(DOMAIN_SUFFIX)
+
+else :
+    DOMAIN_SUFFIX="abc.b.xyz.com"
+    ZONE_ID=get_zoneid(DOMAIN_SUFFIX)
