b2b283c78763268388e3204fa13c8638e52a8db0
Ranjit G <ranjitkumar.518@gmail.com>
2020-07-06 22:27:05 -0700

    Update get_asg_by_env.py

--- get_asg_by_env.py
+++ get_asg_by_env.py
@@ -7,15 +7,15 @@ import time
 
 
 def usage():
-	print("Usage: "+__file__+" env current_region failover_region eg: "+__file__+" prf us-west-2 us-east-2 2")
+	print("Usage: "+__file__+" env current_region eg: "+__file__+" prf us-west-2")
 	exit(1)
 
-def get_asg(env, region, region_type):
+def get_asg(env, region):
 	client = boto3.client('autoscaling', region_name = region)
 	paginator = client.get_paginator('describe_auto_scaling_groups')
 	response_iterator = paginator.paginate()
 	count = 0
-	print "\n Filtering Active ASG in "+failover_region+" for "+env+"\n"
+	print "\n Filtering Active ASG in "+current_region+" for "+env+"\n"
 	for asgs in response_iterator :
 		#import pdb; pdb.set_trace()
 		for each_group in asgs['AutoScalingGroups'] :
@@ -23,8 +23,8 @@ def get_asg(env, region, region_type):
       if env in each_group['AutoScalingGroupName'] and each_group['Tags'][0]['Value'] == 'Active':
 				asg_id = each_group['Tags'][0]['ResourceId']
 				count = count+1
-	if count != 1 and region_type == 'failover':
-		print "\n ******* "+env+" has more than one or No Autoscaling group found Active in "+failover_region+", If required, please deploy ****** \n"
+	if count != 1:
+		print "\n ******* "+env+" has more than one or No Autoscaling group found Active in "+current_region+", If required, please deploy ****** \n"
 		#sys.exit(0)
 
-	return "\n ******** "+env+" has no active ASG in "+failover_region+" ********"
+	return "\n ******** "+env+" has no active ASG in "+current_region+" ********"
