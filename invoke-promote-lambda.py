68f2252b297e705a96d18fd6e2aba70412344583
Ranjit G <ranjitkumar.518@gmail.com>
2020-07-30 19:40:41 -0700

    Create invoke-promote-lambda.py

---
+++ invoke-promote-lambda.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+
+import os
+import json
+import boto3
+import botocore.config
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info('Loading function')
+
+code_pipeline = boto3.client('codepipeline')
+
+def lambda_handler(event, context):
+    try:
+        logger.info('Received complete event {}'.format(event))
+        logger.info('Received event {}'.format(event['CodePipeline.job']['data']['actionConfiguration']))
+        job_data = event['CodePipeline.job']['data']
+        jobId = event['CodePipeline.job']['id']
+
+        if 'UserParameters' not in job_data['actionConfiguration']['configuration']:
+            logger.error("UserParameters not found in pipeline configuration, please ensure to pass all required parameters in UserParameters for Promote stage")
+            sendFailureCodepipeline(jobId, "UserParameters not found in pipeline configuration, please ensure to pass all required parameters in UserParameters for Promote stage")
+            return
+
+        user_parameters = job_data['actionConfiguration']['configuration']['UserParameters']
+        decoded_parameters = json.loads(user_parameters)
+        if not validatePipelineInputParameters(decoded_parameters):
+            logger.error("Failed validation of pipeline input parameters, please ensure to pass all required parameters in UserParameters for Promote stage")
+            sendFailureCodepipeline(jobId, "Failed validation of pipeline input parameters, please ensure to pass all required parameters in UserParameters for Promote stage")
+            return
+
+        asgServiceTag = decoded_parameters['ASGServiceTag']
+        functionARN = decoded_parameters['FunctionARN']
+        region = decoded_parameters['Region']
+        targetAccountName = decoded_parameters['TargetAccountName']
+
+        if targetAccountName == "preprod":
+            crossAccountRoleARN = os.environ['CROSS_ACCOUNT_ROLE_ARN_PREPROD']
+        elif targetAccountName == "commerce-dev":
+            crossAccountRoleARN = os.environ['CROSS_ACCOUNT_ROLE_ARN_COMMERCE_DEV']
+        elif targetAccountName == "estore-global":
+            crossAccountRoleARN = os.environ['CROSS_ACCOUNT_ROLE_ARN_ESTORE']
+        elif targetAccountName == "estore-global-prd":
+            crossAccountRoleARN = os.environ['CROSS_ACCOUNT_ROLE_ARN_ESTORE_PROD']            
+        elif targetAccountName == "prod":
+            crossAccountRoleARN = os.environ['CROSS_ACCOUNT_ROLE_ARN_PROD']
+
+        logger.info('Found Cross Account Role ARN {}'.format(crossAccountRoleARN))
+        sts = boto3.client('sts')
+        assumedRole = sts.assume_role(
+            RoleArn=crossAccountRoleARN,
+            RoleSessionName='LambdaCrossAccountSession'
+        )
+        credentials = assumedRole['Credentials']
+        accessKey = credentials['AccessKeyId']
+        secretAccessKey = credentials['SecretAccessKey']
+        sessionToken = credentials['SessionToken']
+
+        lambdaRequestPayload = dict(ASGServiceTag=asgServiceTag)
+        logger.info('Lambda Request Payload {}'.format(lambdaRequestPayload))
+
+        cfg = botocore.config.Config(connect_timeout=420, read_timeout=420, retries={'max_attempts': 0})
+        lambdaClient = boto3.client('lambda', config=cfg, region_name=region,
+            aws_access_key_id=accessKey,
+            aws_secret_access_key=secretAccessKey,
+            aws_session_token=sessionToken)
+        lambdaResponse = lambdaClient.invoke(
+            FunctionName=functionARN,
+            InvocationType='RequestResponse',
+            Payload=json.dumps(lambdaRequestPayload)
+        )
+        lambdaResponsePayload = json.loads(lambdaResponse['Payload'].read())
+        logger.info('Lambda Response Payload: {}'.format(lambdaResponsePayload))
+        logger.info('Lambda Response StatusCode: {}'.format(lambdaResponse['StatusCode']))
+        if lambdaResponse['StatusCode'] == 200 and 'statusMessage' in lambdaResponsePayload:
+            if lambdaResponsePayload['statusMessage'] == 'promotesuccess':
+                logger.info('Promotion is successful')
+                sendSuccessCodepipeline(jobId, "Promotion is successful")
+            else:
+                logger.info('Promotion failed due to this statusMessage {}'.format(lambdaResponsePayload['statusMessage']))
+                sendFailureCodepipeline(jobId, lambdaResponsePayload['statusMessage'])
+        else:
+            logger.error("Promotion failed either due to non-success status or no status message in response from target account")
+            sendFailureCodepipeline(jobId, "Promotion failed either due to non-success status or no status message in response from target account")
+        
+    except Exception as e:
+        logger.error('Failed: {}'.format(e), exc_info=True)
+        sendFailureCodepipeline(jobId, 'Promotion failed unexpectedly: {}'.format(e))
